0513595ddfde2614afc43fe97e43f36bbe5f3906
EnrMP23 <enrmp2002@gmail.com>
2025-03-15 17:46:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('nav li a, .hero a, .hero-portfolio a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    const targetId = this.getAttribute('href');
+
+    // Solo prevenir el comportamiento por defecto si es un ID interno (ej. #about, #services)
+    if (targetId.startsWith('#') && document.querySelector(targetId)) {
+      e.preventDefault();
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+
